564a13bacdada4e23b6989f2c99d0263e578d50c
Eric Helgeson <erichelgeson@gmail.com>
2021-10-04 16:30:10 -0500

    Test git issues and show rc of easy install

--- stage2/04-install-rascsi/01-run.sh
+++ stage2/04-install-rascsi/01-run.sh
@@ -6,5 +6,9 @@ cd /home/pi/
 git clone https://github.com/akuker/RASCSI.git
 cd RASCSI
 git checkout develop
+git config --global user.email "user@rascsi.com"
+git config --global user.name "RaSCSI User"
+
 ./easyinstall.sh -r=1
+echo "easyinstal exited: $?"
 EOF
